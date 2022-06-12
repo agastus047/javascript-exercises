b5c097c0dccbfabc07132c24c3bc335fff64918a
Agastus Joyson <agastusjoyson@gmail.com>
2022-06-12 19:06:44 +0530

    Completed 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,10 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    const newArray = word.split('');
+    let newWord = '';
+    for (let i = word.length-1; i>=0 ; i--) {
+        newWord+= newArray[i];
+    }
+    return newWord;
 
 };
 
