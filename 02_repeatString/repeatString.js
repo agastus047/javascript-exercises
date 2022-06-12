dc74e65588537939d4d2ae67a2213dd088d69f72
Agastus Joyson <agastusjoyson@gmail.com>
2022-06-12 18:48:46 +0530

    Completed 1&2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,14 @@
-const repeatString = function() {
+const repeatString = function(word,times) {
+    let result = '';
+    if (times < 0) {
+        return "ERROR";
+    }
+    else {
+    for (let i = 0; i < times; i++) {
+        result += word;
+    }
+    return result;
+    }
 
 };
 
