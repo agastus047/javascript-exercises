f761980334253fbbda4716998179c2bc1b53e0c4
Agastus Joyson <agastusjoyson@gmail.com>
2022-06-13 19:01:32 +0530

    Completed 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(...Args) {
+    let argArray = Args;
+    let myArray = argArray.shift();
+    let myArrayNew = myArray.filter(n => !(argArray.includes(n)));
+    return myArrayNew;
+} 
 
 // Do not edit below this line
 module.exports = removeFromArray;
