1d2c60271d25dc4c29d86248b7c325b79c5dc335
Agastus Joyson <agastusjoyson@gmail.com>
2022-06-14 18:32:54 +0530

    Completed 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const ftoc = function() {
+const ftoc = function(tInF) {
+  let tInC = (tInF - 32) * 5/9;
+  let tInC1 = +(tInC.toFixed(1));
+  return tInC1;
 
 };
 
-const ctof = function() {
+const ctof = function(tInC) {
+  let tInF = tInC * 9/5 + 32;
+  let tInF1  = +(tInF.toFixed(1));
+  return tInF1;
 
 };
 
