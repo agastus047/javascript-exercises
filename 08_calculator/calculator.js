297edf2f567a7f8a57ab1bfab79e19b783e6f227
Agastus Joyson <agastusjoyson@gmail.com>
2022-07-15 23:29:50 +0530

    Completed 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
+const add = function(a,b) {
+  return a+b;
 	
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  let sum = 0;
+	for(let i=0;i<a.length;i++) {
+    sum += a[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  let product = 1;
+  if(!(a.length))
+    product = 0;
+  for(let i=0;i<a.length;i++) {
+    product *= a[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n === 0)
+    return 1;
+  let fact = 1;
+  for(let i = n;i>=1;i--) {
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
