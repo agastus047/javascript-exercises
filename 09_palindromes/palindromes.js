259a073ad111321b5615d8920dfffd234ae6efa2
Agastus Joyson <agastusjoyson@gmail.com>
2022-07-16 00:10:05 +0530

    Completed 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (a) {
+    a = a.toLowerCase();
+    a = a.replace(/[^A-Za-z0-9]/g,"");
+    let splitString = a.split('');
+    let reverseArray = splitString.reverse();
+    let joinArray = reverseArray.join("");
+    return (a === joinArray);
+    
 };
 
 // Do not edit below this line
