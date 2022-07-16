d635504a8e39883a5b3de7ad76bde2734a231512
Agastus Joyson <agastusjoyson@gmail.com>
2022-07-16 23:55:40 +0530

    Completed 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest = people[0];
+    let oldestAge = 0;
+    return people.reduce((oldest,person) => {
+        let age;
+        if(!("yearOfDeath" in person)) {
+            age = ((new Date()).getFullYear()) - person.yearOfBirth;
+        }
+        else {
+            age = person.yearOfDeath - person.yearOfBirth;
+        }
+        if(age > oldestAge) {
+            oldest = person;
+            oldestAge = age;
+        }
+        return oldest;
+    },oldest);
 };
 
 // Do not edit below this line
