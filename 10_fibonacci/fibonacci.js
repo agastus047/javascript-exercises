637ff77b9b9f3da498b173cee148e277b627678c
Agastus Joyson <agastusjoyson@gmail.com>
2022-07-16 11:40:41 +0530

    Completed 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    a = +a;
+    if(a<=0 || isNaN(a)) {
+        return "OOPS";
+    }
+    function fib(n) {
+        if(n === 1)
+            return 1;
+        else if(n === 2)
+            return 1;
+        else 
+            return fib(n-2) + fib(n-1);
+    }
+    return fib(a);
 };
 
 // Do not edit below this line
